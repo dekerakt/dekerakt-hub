d91447f33cb548b0d773b7635fcf5c08807fb8ce
LeshaInc <includeurl@gmail.com>
2017-01-02 17:02:38 +0200

    Rewrite echo server using proto/codec/service

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,81 @@
 extern crate futures;
+extern crate tokio_service;
 extern crate tokio_proto;
 extern crate tokio_core;
 
 use std::env;
-use std::net::SocketAddr;
+use std::{io, str};
 
-use futures::Future;
-use futures::stream::Stream;
-use tokio_core::io::{copy, Io};
-use tokio_core::net::TcpListener;
-use tokio_core::reactor::Core;
+
+use tokio_proto::pipeline::ServerProto;
+use tokio_proto::TcpServer;
+
+use tokio_service::Service;
+use tokio_core::io::{Io, Framed, Codec, EasyBuf};
+use futures::future;
+
+struct LineProto;
+
+impl<T: Io + 'static> ServerProto<T> for LineProto {
+    type Request = String;
+    type Response = String;
+    type Error = io::Error;
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = io::Result<Framed<T, LineCodec>>;
+
+    fn bind_transport(&self, io: T) -> io::Result<Framed<T, LineCodec>> {
+        Ok(io.framed(LineCodec))
+    }
+}
+
+struct LineCodec;
+
+impl Codec for LineCodec {
+    type In = String;
+    type Out = String;
+
+    fn decode(&mut self, buf: &mut EasyBuf) -> io::Result<Option<String>> {
+        if let Some(pos) = buf.as_ref().iter().position(|&b| b == b'\n') {
+            let line = buf.drain_to(pos);
+            buf.drain_to(1);
+
+            return match str::from_utf8(line.as_ref()) {
+                Ok(v) => Ok(Some(v.to_string())),
+                Err(_) =>
+                    Err(io::Error::new(io::ErrorKind::Other, "invalid string"))
+            };
+        }
+
+        Ok(None)
+    }
+
+    fn encode(&mut self, msg: String, buf: &mut Vec<u8>) -> io::Result<()> {
+        for &byte in msg.as_bytes() {
+            buf.push(byte);
+        }
+
+        buf.push(b'\n');
+        Ok(())
+    }
+}
+
+struct EchoService;
+
+impl Service for EchoService {
+    type Request = String;
+    type Response = String;
+    type Error = io::Error;
+    type Future = future::Ok<String, io::Error>;
+
+    fn call(&mut self, req: String) -> Self::Future {
+        future::ok(req)
+    }
+}
 
 fn main() {
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
     let addr = addr.parse().unwrap();
 
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-
-    let socket = TcpListener::bind(&addr, &handle).unwrap();
-    println!("Listening on {}", addr);
-
-    let done = socket.incoming().for_each(|(socket, addr)| {
-        let (reader, writer) = socket.split();
-        let amt = copy(reader, writer);
-
-        let msg = amt.then(move |result| {
-            match result {
-                Ok(amt) => println!("wrote {} bytes to {}", amt, addr),
-                Err(e) => println!("error on {}: {}", addr, e),
-            }
-
-            Ok(())
-        });
-
-        handle.spawn(msg);
-
-        Ok(())
-    });
-
-    core.run(done).unwrap();
+    TcpServer::new(LineProto, addr)
+        .serve(|| Ok(EchoService));
 }
