eb4df3b9aec01bd4138308275deffce7c5daa48e
LeshaInc <includeurl@gmail.com>
2017-01-05 19:22:11 +0200

    Encode Message::InitalData

--- src/codec/encoder.rs
+++ src/codec/encoder.rs
@@ -2,6 +2,8 @@ use std::io::Write;
 use byteorder::{BigEndian, WriteBytesExt};
 
 use message::{Message, AuthResult};
+use graphics::{Color, Palette, ScreenResolution, ScreenState,
+    PreciseMode, Char};
 
 pub trait EncodeExt: Write {  // No error handling
     fn encode_u8(&mut self, n: u8) {
@@ -33,6 +35,49 @@ pub trait EncodeExt: Write {  // No error handling
         self.write_all(string.as_ref()).unwrap();
     }
 
+    fn encode_screen_state(&mut self, screen_state: ScreenState) {
+        self.encode_u8(screen_state as u8)
+    }
+
+    fn encode_precise_mode(&mut self, precise_mode: PreciseMode) {
+        self.encode_u8(precise_mode as u8)
+    }
+
+    fn encode_screen_resolution(&mut self, screen_resolution: ScreenResolution) {
+        self.encode_u8(screen_resolution.width);
+        self.encode_u8(screen_resolution.height)
+    }
+
+    fn encode_color(&mut self, color: Color) {
+        self.encode_u8(color.r);
+        self.encode_u8(color.g);
+        self.encode_u8(color.b)
+    }
+
+    fn encode_palette(&mut self, palette: Palette) {
+        for &color in palette.iter() {
+            self.encode_color(color);
+        }
+    }
+
+    fn encode_char_raw(&mut self, char_raw: char) {
+        let mut buf = [0; 4];
+        char_raw.encode_utf8(&mut buf);
+        self.write_all(&buf).unwrap();
+    }
+
+    fn encode_char(&mut self, char: Char) {
+        self.encode_u8(char.fg_idx);
+        self.encode_u8(char.bg_idx);
+        self.encode_char_raw(char.char);
+    }
+
+    fn encode_chars(&mut self, chars: Vec<Char>) {
+        for char in chars {
+            self.encode_char(char);
+        }
+    }
+
     fn encode_message(&mut self, message: Message) {
         let ref mut body = Vec::with_capacity(8);
         let code;
@@ -52,6 +97,26 @@ pub trait EncodeExt: Write {  // No error handling
                 code = 0x02;
                 body.encode_auth_result(auth_result);
                 body.encode_string(display_message);
+            }
+
+            Message::InitialData {
+                palette,
+                fg_idx,
+                bg_idx,
+                resolution,
+                screen_state,
+                precise_mode,
+                chars
+            } => {
+                code = 0x03;
+
+                body.encode_palette(palette);
+                body.encode_u8(fg_idx);
+                body.encode_u8(bg_idx);
+                body.encode_screen_resolution(resolution);
+                body.encode_screen_state(screen_state);
+                body.encode_precise_mode(precise_mode);
+                body.encode_chars(chars);
             }
 
             _ => unimplemented!()
